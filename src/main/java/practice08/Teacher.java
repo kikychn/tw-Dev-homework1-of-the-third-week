a7e1498f76f4905b06047b653f585589915a3117
kikychn <kikychn@gmail.com>
2019-04-12 19:56:17 +0800

    finish the class of Teacher

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,38 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        if (klass != null) {
+            return super.introduce() + " I am a Teacher. I teach " + klass.getDisplayName() + ".";
+        } else {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().getNumber() == klass.getNumber()) {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        } else {
+            return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+        }
+    }
 }
