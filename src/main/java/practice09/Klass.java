0446bc088e2994dde84fa05b0042e9c3756984a0
kikychn <kikychn@gmail.com>
2019-04-12 20:28:45 +0800

    finish practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,49 @@
 package practice09;
 
+import java.util.Objects;
+
 public class Klass {
+    private Integer number;
+    private Student leader;
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return number.equals(klass.number);
+    }
+
+
+    public void assignLeader(Student leader) {
+        if (this.equals(leader.getKlass())) {
+            this.leader = leader;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student){
+        student.setKlass(this);
+    }
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
 }
