00d44ca94ae98792b10559142bef379fdf792340
kikychn <kikychn@gmail.com>
2019-04-12 14:49:19 +0800

    finish the class of Teacher

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,30 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        if(getKlass()!=null){
+            return super.introduce() + " I am a Teacher. I teach Class " + getKlass() + ".";
+        }else {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+    }
 }
