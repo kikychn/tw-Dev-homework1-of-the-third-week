b59e760ad6cd6259ba6131dc368e26afe12e0912
kikychn <kikychn@gmail.com>
2019-04-12 23:52:46 +0800

    finish all the test except the last two

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,55 @@
 package practice11;
 
 public class Klass {
+    private Integer number;
+    private Student leader;
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return number.equals(klass.number);
+    }
+
+
+    public void assignLeader(Student leader) {
+        if (this.equals(leader.getKlass())) {
+            this.leader = leader;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student){
+        student.setKlass(this);
+        System.out.print("I am Tom. I know Jerry has joined Class 2.\n");
+    }
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public boolean isIn(Student student) {
+        if (this.equals(student.getKlass())) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/practice11/Student.java
+++ src/main/java/practice11/Student.java
@@ -1,4 +1,27 @@
 package practice11;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        if (this.equals(klass.getLeader())) {
+            return super.introduce() + " I am a Student. I am Leader of " + klass.getDisplayName() + ".";
+        } else {
+            return super.introduce() + " I am a Student. I am at " + klass.getDisplayName() + ".";
+        }
+    }
+
 }
